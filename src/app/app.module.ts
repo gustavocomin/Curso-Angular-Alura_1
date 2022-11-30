dd4675cc0a02c11bc557505ac63f4d46d7f81390
gustavo.comin <gustavofcomin@gmail.com>
2022-11-29 22:36:03 -0300

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
+import AppComponent from "./app.component";
+import { PhotosModule } from "./photos/photos.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, PhotosModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/photos/photos.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { PhotoListComponent } from "./photo-list/photo-list.component";
+import { PhotoComponent } from "./photo/photo.component";
+
+@NgModule({
+  declarations: [PhotoComponent, PhotoListComponent],
+  imports: [HttpClientModule, CommonModule],
+})
+export class PhotosModule {}
