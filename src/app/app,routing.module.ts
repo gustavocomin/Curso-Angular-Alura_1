a6f41570623aab9b3beb4a5e7be429ff47acced2
gustavo.comin <gustavofcomin@gmail.com>
2022-12-06 23:20:25 -0300

    .

---
+++ src/app/app,routing.module.ts
@@ -0,0 +1,24 @@
+import { PhotoListResolver } from "./photos/photo-list/photo-list.resolver";
+import { PhotoFormComponent } from "./photos/photo-form/photo-form.component";
+import { PhotoListComponent } from "./photos/photo-list/photo-list.component";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { NotFoundComponent } from "./errors/not-found/not-found.component";
+
+const routes: Routes = [
+  {
+    path: "user/:userName",
+    component: PhotoListComponent,
+    resolve: {
+      photos: PhotoListResolver,
+    },
+  },
+  { path: "p/add", component: PhotoFormComponent },
+  { path: "**", component: NotFoundComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -3,9 +3,13 @@ import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { PhotoListComponent } from "./photo-list/photo-list.component";
 import { PhotoComponent } from "./photo/photo.component";
+import { PhotoFormComponent } from "./photo-form/photo-form.component";
+import { PhotosComponent } from './photo-list/photos/photos.component';
+import { FilterByDescription } from './photo-list/filter-by-description.pipe';
+import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
 
 @NgModule({
-  declarations: [PhotoComponent, PhotoListComponent],
+  declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent, FilterByDescription, LoadButtonComponent],
   imports: [HttpClientModule, CommonModule],
 })
 export class PhotosModule {}
